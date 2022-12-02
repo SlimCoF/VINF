e44a895d26c14cf7a098d375563539d02accfaca
Filip Slimák <xslimak@stuba.sk>
2022-12-02 17:10:21 +0100

    update

--- searching.py
+++ searching.py
@@ -1,93 +1,108 @@
 import requests
-from process_data import lemmatize_and_stem
 import re
-import operator
+import lucene
 
-def search_id(title, original_data_json):
+from org.apache.lucene.search import IndexSearcher
+from org.apache.lucene.index import DirectoryReader
+from org.apache.lucene.queryparser.classic import QueryParser
+from org.apache.lucene.search.similarities import BM25Similarity
+from org.apache.lucene.store import FSDirectory
+from org.apache.lucene.analysis.en import EnglishAnalyzer
+lucene.initVM()
+
+from java.io import File
+
+class color:
+   BOLD = '\033[1m'
+   END = '\033[0m'
+
+INDEX_PATH = "./index"
+index_dir = FSDirectory.open(File(INDEX_PATH).toPath())
+searcher = IndexSearcher(DirectoryReader.open(index_dir))
+searcher.setSimilarity(BM25Similarity(1.2, 0.75))
+
+
+def searchAltNames(term):
+    query = QueryParser("alt_names", EnglishAnalyzer()).parse(term)
+    query_output = searcher.search(query, 10).scoreDocs
+    output = []
+    for item in query_output:
+        doc = searcher.doc(item.doc)
+        output.append((doc.get("title_id"), doc.get("alt_names").split(";")))
+    return output
+
+
+def searchTitle(title_id):
+    query = QueryParser("title_id", EnglishAnalyzer()).parse(title_id)
+    query_output = searcher.search(query, 1).scoreDocs
+    output = []
+    for item in query_output:
+        doc = searcher.doc(item.doc)
+        output.append((doc.get("title_id"), doc.get("alt_names").split(";")))
+    return output
+
+
+def getTitle(title_id):
+    wiki_url = f"https://www.wikidata.org/wiki/{title_id}"
+    response = requests.get(wiki_url)
+    title = re.search(r'(<title>).*(<\/title>)', response.text)
+    return title.group(0)[7:-19]
+
+
+def getTitleId(title):
     title = title.replace(" ", "+")
-    search_url = f"https://www.wikidata.org/w/index.php?search=%22{title}%22&title=Special:Search&profile=advanced&fulltext=1&ns0=1&ns120=1"
-    response = requests.get(search_url)
+    wiki_url = f"https://www.wikidata.org/w/index.php?search=%22{title}%22&title=Special:Search&profile=advanced&fulltext=1&ns0=1&ns120=1"
+    response = requests.get(wiki_url)
     response = response.text.split("<div class=\"mw-search-result-heading\">")
     if len(response) <= 1:
         return None
     heading_id = response[1].split("\"")[1][6:]
-
-    if heading_id in list(original_data_json.keys()):
-        print(f"Hladany vyraz: \"{title.replace('+', ' ')}\" sa nachadza v databaze pod ID: {heading_id}")
-        print(f"Alternativne mena pre vyraz \"{title.replace('+', ' ')}\":")
-        for alt_name in original_data_json[heading_id]:
-            print(f"    {alt_name}")
-    elif heading_id:
-        print(f"Hladany vyraz: {title}, s ID: {heading_id} sa v databaze nenachadza")
-    else:
-        print(f"Pre hladany vyraz: {title} neexistuje ID")
-
-    # return heading_id
+    return heading_id
 
 
-def request_heading(id):
-    search_url = f"https://www.wikidata.org/wiki/{id}"
-    response = requests.get(search_url)
-    title = re.search(r'(<title>).*(<\/title>)', response.text)
+def showOutput(searchOutput):
+    for item in searchOutput:
+        title = getTitle(item[0])
+        print(f"Link: {color.BOLD}https://en.wikipedia.org/wiki/{title.replace(' ', '_')}{color.END}")
+        print(f"Nazov: {color.BOLD}{title}{color.END}")
+        print("Alternativne mena:")
+        print("    " + "\n    ".join(item[1][:10]))
 
-    return title.group(0)[7:-19]
 
+if __name__ == "__main__":
 
-def search_alt_name(alt_name, unique_words, original_data_json):
-    title_ids = {}
-    processed_words = []
-    operation = 0  # 0 - OR, 1 - AND
+    option = ""
+    while option.lower() != "e":
+        print("--------------------------------------------")
+        option = input("H - Hladanie podla nazvu\n" +
+                       "A - Hladanie v alternativnych menach\n" +
+                       "E - Ukoncenie\n")
 
-    if re.search("^\"((?!\").)*\"$", alt_name):
-        alt_name = alt_name[1:-1]
-        operation = 1
+        if option.lower() == "h":
+            title = input("Zadaj nazov:\n")
+            title_id = getTitleId(title)
+            if title_id == None:
+                print("Neexistuje clanok s hladanym nazvom!!")
+                continue;
+            searchOutput =  searchTitle(title_id)
+            if searchOutput == []:
+                print(f"Nazov: {color.BOLD}{title}{color.END} , id: {color.BOLD}{title_id}{color.END} sa nenachadza v databaze")
+                continue;
+            print(f"Vysledok hladania pre nazov: {title}")
+            showOutput(searchOutput)
 
-    for word in alt_name.split(" "):
-        s_w = lemmatize_and_stem(word)
-        if s_w not in processed_words:
-            if s_w not in unique_words:
-                print(f"slovo: \"{word}\" (po uprave \"{s_w}\") sa v slovniku nenachadza")
-            else:
-                processed_words.append(s_w)
+        elif option.lower() == "a":
+            term = input("Zadaj vyraz:\n")
+            searchOutput = searchAltNames(term)
+            if searchOutput == []:
+                print(f"Pre vyraz: {color.BOLD}{term}{color.END} sa nenasla ziadna zhoda")
+                continue;
+            print(f"Vysledok hladania pre vyraz: {term}")
+            showOutput(searchOutput)
 
-                if len(title_ids) == 0:
-                    title_ids = unique_words[s_w]
-                else:
-                    set1 = set(title_ids)
-                    set2 = set(unique_words[s_w])
-                    if operation == 1:
-                        title_ids = {
-                            key: title_ids.get(key, 0) + unique_words[s_w].get(key, 0) for key in set1 & set2
-                        }
-                    elif operation == 0:
-                        title_ids = {
-                            key: title_ids.get(key, 0) + unique_words[s_w].get(key, 0) for key in set1 | set2
-                        }
+        elif option.lower() == "e":
+            break
 
-    if len(processed_words) == 0:
-        return None
-
-    title_ids = dict(sorted(title_ids.items(), key=operator.itemgetter(1), reverse=True))
-    output = list(title_ids.keys())
-
-    # AND
-    if operation == 1:
-        print(f'Vysledok hladania alternativnych mien pre zadany vyraz: "{alt_name}"')
-        for item in output:
-            title = request_heading(item)
-            print(f'Title: "{title}", ID: {item}, TF: {title_ids[item]}')
-            print("    " + '\n    '.join(original_data_json[item]))
-            print("\n")
-
-        return output
-
-    # OR
-    elif operation == 0:
-        print(f"Vysledok hladania alternativnych mien pre zadany vyraz: {alt_name}")
-        for item in output:
-            title = request_heading(item)
-            print(f'Title: "{title}", ID: {item}, TF: {title_ids[item]}')
-            print("    " + '\n    '.join(original_data_json[item]))
-            print("\n")
-
-        return output
+        else:
+            print("Pre zadany znak neexistuje funkcionalita!!")
+         
